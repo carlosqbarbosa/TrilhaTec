511a645897e0a390086fd24655dc94743fb6f226
Carlosbarbosa <carloshenrique29.qb@gmail.com>
2026-09-08 21:00:47 -0300

    Python: aula sobre representação de dados

---
+++ Python1/Aula 05 - Miniprojeto (Compreender a Representação de Dados)/app.py
@@ -0,0 +1,12 @@
+n1 = float(input("Digite o número 1: "))
+n2 = float(input("digie o numéro 2: "))
+
+soma = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+
+print(f"A soma é {soma}")
+print(f"A subtração é {sub}")
+print(f"A multiplicação é {mul}")
+print(f"A divisão é {div}")
